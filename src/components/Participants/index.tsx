e9e8d2d574682d1c6361969579fb2a8f6ef44480
Altencir Junior <altencirsilvajr@gmail.com>
2023-01-17 19:23:49 -0300

    first commit

---
+++ src/components/Participants/index.tsx
@@ -0,0 +1,26 @@
+import { Text,View, TouchableOpacity } from "react-native";
+import { styles } from "./style";
+import React from 'react'
+
+
+type Props = {
+	name: string;
+  onRemove: () => void;
+}
+
+export function Participants({name, onRemove}: Props){
+    return(
+        <View style = {styles.container}>
+            <Text style ={styles.name}>
+              {name}
+            </Text>
+
+            <TouchableOpacity 
+            style={styles.button} onPress ={onRemove}>
+              <Text style={styles.buttonText}>
+              -
+            </Text>
+          </TouchableOpacity>
+        </View>
+    )
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,92 @@
+import React, {useState} from "react";
+
+import { View , Text, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
+
+import { styles } from "./style";
+
+import { Participants } from "../../components/Participants";
+
+export function Home() {
+  const [participantName , setParticipantName] = useState('');
+  const [participants , setParticipants] = useState <String[]> ([]);
+
+    function handleParticipantAdd() {
+      
+      const participantsLowerCase = participants.map(participant => participant.toLowerCase())
+      if (participantsLowerCase.includes(participantName.toLowerCase())) {
+        return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.");
+      }
+      
+      if (participantName.length === 0) {
+        return Alert.alert("Espaço vazio", "Nenhum valor foi digitado");
+
+      }
+      setParticipants( previewState => [...previewState ,participantName]);
+      setParticipantName ('');
+
+    }
+    
+
+  function handleParticipantRemove(name: string) {
+    Alert.alert("Remover",`Deseja realmente remover: ${name}?`,
+    [
+      {
+        text: "SIM",
+        onPress: () => setParticipants(previewState => previewState.filter(participants => participants !== name))
+      },
+      {
+        text:"NÃO",
+        style: "cancel"
+      }
+    ])
+
+  }
+  return(
+    <View style={styles.container}>
+        <Text style={styles.eventTitle}>
+          Nome do Evento
+        </Text>
+
+        <Text style={styles.eventDate}>
+          27 de Janeiro de 2023.
+        </Text>
+
+        <View style={styles.form}>
+
+          <TextInput 
+            style={styles.input}
+            placeholder = "Nome do participante"
+            placeholderTextColor= "#6B6B6B"
+            onChangeText={setParticipantName}
+            value ={participantName}
+          />
+          
+          <TouchableOpacity 
+            style={styles.button} 
+            onPress={handleParticipantAdd}>
+              <Text style={styles.buttonText}>
+              +
+            </Text>
+          </TouchableOpacity>
+    
+        </View>
+        
+        <FlatList showsVerticalScrollIndicator ={false}
+         
+          data={participants}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Participants
+                key={item}
+                name = {item} 
+                onRemove ={()=> handleParticipantRemove(`${item}`)}/>
+          )}
+        ListEmptyComponent={() => (
+          <Text style ={styles.listEmpty}>
+            Nenhum participante adicionado ao evento!!!
+          </Text>
+        )}  
+        />
+    </View>
+  );
+}
